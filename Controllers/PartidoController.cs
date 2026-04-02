6b3df71e2511fa5cadc72c4e27fcf3d621da5f0d
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-02 10:04:35 -0500

    commit - PartidoController .

--- Controllers/PartidoController.cs
+++ Controllers/PartidoController.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using sistema_de_partidos.Data;
+using sistema_de_partidos.Models;
+using sistema_de_partidos.Services;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace sistema_de_partidos.Controllers
 {
-    internal class PartidoController
+    public class PartidoController
     {
-    }
+        private readonly PartidoDAO dao;
+        private readonly ResultadoService service;
+
+        public PartidoController(string connectionString)
+        {
+            dao = new PartidoDAO(connectionString);
+            service = new ResultadoService();
+        }
+
+        public void Crear(Partido p)
+        {
+            p.Resultado = service.CalcularResultado(p.GolesLocal, p.GolesVisitante);
+            dao.Insertar(p);
+        }
+
+        public List<Partido> Listar()
+        {
+            return dao.Listar();
+        }
+
+        public void Actualizar(Partido p)
+        {
+            p.Resultado = service.CalcularResultado(p.GolesLocal, p.GolesVisitante);
+            dao.Actualizar(p);
+        }
+
+        public void Eliminar(int id)
+        {
+            dao.Eliminar(id);
+        }
+    }   
 }
