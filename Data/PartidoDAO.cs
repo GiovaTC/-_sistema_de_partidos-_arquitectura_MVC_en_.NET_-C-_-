068a7e3cb4523563af153f41483b0f3852d83c20
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-02 09:40:21 -0500

    commit - PartidoDAO .

--- Data/PartidoDAO.cs
+++ Data/PartidoDAO.cs
@@ -1,4 +1,5 @@
 ﻿using Oracle.ManagedDataAccess.Client;
+using sistema_de_partidos.Models;
 using System;
 using System.Collections.Generic;
 
@@ -6,5 +7,34 @@ namespace sistema_de_partidos.Data
 {
     public class PartidoDAO
     {
+        private readonly string connectionString;
+
+        public PartidoDAO(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void Insertar(Partido p)
+        {
+            using (var conn = new OracleConnection(connectionString))
+            {
+                conn.Open();
+
+                string sql = @"INSERT INTO PARTIDOS
+                                (EQUIPO_LOCAL, EQUIPO_VISITANTE, GOLES_LOCAL, GOLES_VISITANTE, RESULTADO)
+                                VALUES (:local, :visitante, :gl, :gv, :res)";
+
+                using (var cmd = new OracleCommand(sql, conn))
+                {
+                    cmd.Parameters.Add(":local", p.EquipoLocal);
+                    cmd.Parameters.Add(":visitante", p.EquipoVisitante);
+                    cmd.Parameters.Add(":gl", p.GolesLocal);
+                    cmd.Parameters.Add(":gv", p.GolesVisitante);
+                    cmd.Parameters.Add(":res", p.Resultado);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
