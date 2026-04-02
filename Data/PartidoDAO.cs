409c46b38138853728e91fd63fca68718c826019
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-02 09:05:44 -0500

    commit - Program .

---
+++ Data/PartidoDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistema_de_partidos.Data
+{
+    internal class PartidoDAO
+    {
+    }
+}
