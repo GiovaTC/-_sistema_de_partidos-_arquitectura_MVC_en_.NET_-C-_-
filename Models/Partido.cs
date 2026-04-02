038f214752ebf996bb68800001072374b542348c
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-02 09:09:04 -0500

    commit - Partido .

--- Models/Partido.cs
+++ Models/Partido.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace sistema_de_partidos.Models
 {
-    internal class Partido
+    public class Partido
     {
-    }
+        public int Id { get; set; }
+        public string EquipoLocal { get; set; }
+        public string EquipoVisitante { get; set; }
+        public int GolesLocal { get; set; }
+        public int GolesVisitante { get; set; }
+        public string Resultado { get; set; }
+    }   
 }
